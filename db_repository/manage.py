617a48afa2ef59ab8912ff5f759b49f15f12eed8
fdrozdowski <fgd1@rice.edu>
2014-11-01 16:49:43 -0500

    Revert "After login renders temp.html"

--- db_repository/manage.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-from migrate.versioning.shell import main
-
-if __name__ == '__main__':
-    main(six='<module 'six' from '/Users/fdrozdowski/Documents/Rice/Riceapps/microblog/flask/lib/python3.4/site-packages/six.py'>')
