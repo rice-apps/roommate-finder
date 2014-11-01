bc8613702f2d753e137a964e73bfca4a20c1fd6e
fdrozdowski <fgd1@rice.edu>
2014-11-01 16:11:07 -0500

    Added welcome page after CAS login

--- app/views.py
+++ app/views.py
@@ -1,18 +1,44 @@
-
-from app import app
-from flask import render_template, flash, redirect
-from .forms import LoginForm
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from app import app, db, lm
+from app.models import User
+from app.forms import LoginForm
 
 app.config['CAS_SERVER'] = 'https://netid.rice.edu'
-app.config['CAS_AFTER_LOGIN'] = 'index'
+app.config['CAS_AFTER_LOGIN'] = 'after_login'
+app.config.setdefault('CAS_USERNAME_SESSION_KEY', 'CAS_USERNAME')
 
+@app.route('/after_login')
+def after_login():
+    login = session.get(app.config['CAS_USERNAME_SESSION_KEY'], None)
+
+    user = User.query.filter_by(nickname=login).first()
+    if user is None:
+        nickname = login
+        user = User(nickname=nickname, email=nickname+"@rice.edu")
+        db.session.add(user)
+        db.session.commit()
+
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user, remember = remember_me)
+
+    posts = []
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           posts=posts)
+        #redirect(url_for('index'))
 
 @app.route('/')
 @app.route('/index')
 
 def index():
 
-    user = {'nickname': 'Miguel'}  # fake user
+    login = session.get(app.config['CAS_USERNAME_SESSION_KEY'], None)
+    user = {'nickname': login}  # fake user
     posts = [  # fake array of posts
         {
             'author': {'nickname': 'John'},
@@ -23,14 +49,17 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return app.send_static_file('index.html')
-    """return render_template("index.html",
+
+    return render_template("index.html",
                            title='Home',
                            user=user,
-                           posts=posts)"""
+                           posts=posts)
 
 
+
+"""
 @app.route('/login', methods=['GET', 'POST'])
+
 def login():
     form = LoginForm()
     if form.validate_on_submit():
@@ -41,3 +70,8 @@ def login():
                            title='Sign In',
                            form=form,
                            providers=app.config['OPENID_PROVIDERS'])
+"""
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
