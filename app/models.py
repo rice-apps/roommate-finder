6187baea40e24cb0d511e689e397a4c171d454ff
fdrozdowski <fgd1@rice.edu>
2014-10-31 20:41:39 -0500

    Initial skeleton - Flask

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-"""from app import db
+from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -6,4 +6,4 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
 
     def __repr__(self):
-        return '<User %r>' % (self.nickname)"""
+        return '<User %r>' % (self.nickname)
--- main.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'fdrozdowski'
-
-#!flask/bin/python
-from app import app
-app.run(debug=True)
