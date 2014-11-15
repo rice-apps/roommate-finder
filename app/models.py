e2ed4d33b5160ade4ac764dcea5e9de2cfafbd59
LINKIWI <developer@kevinlin.info>
2014-11-15 00:23:37 -0600

    Implemented storage of profiles in user database

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from app import db
 
-class Profiles(db.Model):
+class Profile(db.Model):
         # Columns for user profile table
         net_id = db.Column(db.String(64), primary_key = True, unique = True, index = True)
         name = db.Column(db.String(255))
@@ -10,5 +10,14 @@ class Profiles(db.Model):
         gender = db.Column(db.String(255))
         bio = db.Column(db.String(255))
 
+        def __init__(self, net_id, name, year, age, college, gender, bio):
+            self.net_id = net_id
+            self.name = name
+            self.year = year
+            self.age = age
+            self.college = college
+            self.gender = gender
+            self.bio = bio
+
         def __repr__(self):
-                return '<User %r>' % (self.net_id)
+                return '<User %r>' % self.net_id
--- app/views.py
+++ app/views.py
@@ -1,15 +1,14 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm
-from app.models import Profiles
+from app.models import Profile
 from app.forms import LoginForm
 
 app.config['CAS_SERVER'] = 'https://netid.rice.edu'
 app.config['CAS_AFTER_LOGIN'] = 'after_login'
 app.config.setdefault('CAS_USERNAME_SESSION_KEY', 'CAS_USERNAME')
 
-
-@app.route('/after_login')
+@app.route('/after_login', methods=['GET'])
 def after_login():
     # User Net ID
     login = session.get(app.config['CAS_USERNAME_SESSION_KEY'], None)
@@ -18,23 +17,36 @@ def after_login():
     data = {"net_id": login}
 
     # Try to find Net ID in database
-    user = Profiles.query.filter_by(net_id=login).first()
+    user = Profile.query.filter_by(net_id=login).first()
     if user is None:
         # User doesn't exist in DB
         # Redirect the user to the profile creation page
+        print("User does not exist")
         return render_template('profile_creation.html', data=data)
-    # return render_template("profile_creation.html", data=data)
-    #nickname = login
-    #user = User(nickname=nickname, email=nickname+"@rice.edu")
-    #db.session.add(user)
-    #db.session.commit()
     else:
         # User does exist in DB
         # Redirect user to main page
+        print("User does exist")
         return app.send_static_file('intro.html')
 
+@app.route('/createprofile', methods=['POST'])
+def create_user():
+    """
+    Processes form data in POST request and adds the user to the database table Profiles.
+    """
+    net_id = request.form['net_id']
+    name = request.form['name']
+    year = request.form['year']
+    age = request.form['age']
+    college = request.form['college']
+    gender = request.form['gender']
+    bio = request.form['bio']
+    user = Profile(net_id, name, year, age, college, gender, bio)
+    db.session.add(user)
+    db.session.commit()
+    return app.send_static_file('intro.html')
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     login = session.get(app.config['CAS_USERNAME_SESSION_KEY'], None)
     user = {'nickname': login}
@@ -48,4 +60,4 @@ def about():
 
 @lm.user_loader
 def load_user(id):
-    return User.query.get(int(id))
+    return Profiles.query.get(int(id))
