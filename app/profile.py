1f381f4c86b8e8883e744a846dd6825fd2ce0d84
Kevin Lin <developer@kevinlin.info>
2014-12-18 16:51:37 -0800

    Fix UI bugs, clean up code, initial FB integration

---
+++ app/profile.py
@@ -0,0 +1,122 @@
+# profile.py
+# This file contains all methods related to manipulation of the user profile in the database.
+# This includes creating a new user, modifying an existing user, and deleting a user.
+
+
+import os
+
+from flask import render_template, session, request
+from werkzeug.utils import secure_filename, redirect
+
+from app import app, db
+from app.models import Profile
+
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif', 'tiff'])
+
+
+@app.route('/createprofile', methods=['POST'])
+def create_user():
+    """
+    Processes form data in POST request from profile creation page and adds the user to the database table Profile.
+
+    The profile photo associated with the user is recorded in the column "photo" in the Profile table.
+    It is stored as <hash>.<file_extension> where <hash> is a hash is a hash of the uploaded photo and <file_extension> is...well, the file extension.
+    The actual photo file is stored as app/photos/<hash>.<file_extension>
+    """
+    # Fields from form
+    fields = ["net_id", "name", "year", "dob", "college", "gender", "bio", "facebook"]
+    # Get user-entered values from form
+    values = {}
+    for field in fields:
+        values[field] = request.form[field]
+    # Uploaded profile photo
+    photo = request.files['photo']
+    # Generate a string representation of a hash of the photo
+    # This associates the local copy of the file with the user in the database
+    photo_hash = str(hash(photo))
+    # Check for Facebook account connection: if the user connected an account, then the ID is a string of
+    # numbers without spaces; otherwise, it's just the placeholder text, which should be None
+    if values["facebook"].count(" ") > 0:
+        values["facebook"] = None
+    # Create a new user from the Profile model
+    if photo:
+        user = Profile(values["net_id"], values["name"], values["year"], values["dob"], values["college"], values["gender"], values["bio"], values["facebook"], photo_hash + "." + file_extension(photo.filename))
+    else:
+        user = Profile(values["net_id"], values["name"], values["year"], values["dob"], values["college"], values["gender"], values["bio"], values["facebook"])
+    # The user selected a photo of an invalid file extension
+    # Redirect the user to an error page
+    if photo and not allowed_file(photo.filename):
+        data = {"net_id": values["net_id"], "name": values["name"], "year": values["year"], "dob": values["dob"], "college": values["college"], "gender": values["gender"], "bio": values["bio"], "facebook": values["facebook"], "error": "Error: invalid photo file extension ." + str(file_extension(photo.filename))}
+        return render_template('profile_creation.html', data=data)
+    # Store the user's profile photo on the server
+    if photo and allowed_file(photo.filename):
+        filename = secure_filename(photo_hash) + "." + file_extension(photo.filename)
+        photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    # Add this new user to the database
+    db.session.add(user)
+    db.session.commit()
+    data = {"net_id": values["net_id"], "profile": user}
+    return render_template('my_profile.html', data=data)
+
+
+@app.route('/updateprofile', methods=['POST'])
+def update_user():
+    """
+    Called when the user is modifying is/her account from the My Profile page.
+    Updates the database to reflect the user's changes.
+    """
+    # Retrieve the user by net ID from the database
+    user = Profile.query.filter_by(net_id=request.form["net_id"]).first()
+    # Update all the columns
+    user.name = request.form["name"]
+    user.year = request.form["year"]
+    user.dob = request.form["dob"]
+    user.college = request.form["college"]
+    user.gender = request.form["gender"]
+    user.bio = request.form["bio"]
+    # Update the photo only if the user has changed the picture
+    photo = request.files['photo']
+    if photo and allowed_file(photo.filename):
+        photo_hash = str(hash(request.files['photo']))
+        filename = secure_filename(photo_hash) + "." + file_extension(photo.filename)
+        photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        user.photo = photo_hash + "." + file_extension(photo.filename)
+    # Commit the changes
+    db.session.merge(user)
+    db.session.commit()
+    # Pass along the data after refresh
+    data = {"net_id": user.net_id, "profile": user}
+    return render_template('my_profile.html', data=data)
+
+
+@app.route('/deleteprofile', methods=['GET', 'POST'])
+def delete_user():
+    """
+    Removes the user from the database.
+    """
+    # Get Net ID from GET data
+    net_id = request.args.get('net_id', '')
+    # Security measure - make sure the Net ID of the user whose deletion was requested
+    # matches with the Net ID of the user currently logged in
+    # This is a pretty lame security measure in all truth
+    if session.get(app.config['CAS_USERNAME_SESSION_KEY'], None) == net_id:
+        user = Profile.query.filter_by(net_id=net_id).first()
+        db.session.delete(user)
+        db.session.commit()
+    # Logout the session after profile deletion.
+    return redirect("/logout", code=302)
+
+
+def allowed_file(filename):
+    """
+    Check if the file is of a permissible file extension.
+    """
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def file_extension(filename):
+    """
+    Get the file extension of file. Excludes the "." before the extension.
+    """
+    return filename.rsplit('.', 1)[1]
--- app/views.py
+++ app/views.py
@@ -1,19 +1,21 @@
-import os
+# views.py
+# This file contains all methods related to routing, and directing the user to the correct location.
+
+
 import urllib2
-
-from flask import render_template, session, request, send_from_directory
 import time
-from werkzeug.utils import secure_filename, redirect
 
-from app import app, db, lm
+from flask import render_template, session, send_from_directory
+from werkzeug.utils import redirect
+
+from app import app, lm
 from app.models import Profile
 
 
-# This needs to be an absolute path. That's so stupid.
+# Server upload folder - do not change
 # UPLOAD_FOLDER = "Z:/RoommateFinder/roommate-finder/app/photos"
-# Upload folder for local development environment.
+# Local dev environment upload folder - change as necessary
 UPLOAD_FOLDER = "D:/GitHub/roommate-finder/app/photos"
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app.config['CAS_SERVER'] = 'https://netid.rice.edu'
 app.config['CAS_AFTER_LOGIN'] = 'after_login'
@@ -31,6 +33,25 @@ def add_header(response):
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
     response.headers['Cache-Control'] = 'public, max-age=0'
     return response
+
+
+@app.route('/', methods=['GET'])
+def index():
+    """
+    First check if the user is logged in. If so, redirect him/her to the main search page.
+    If not, redirect him/her to the intro page.
+    """
+    net_id = session.get(app.config['CAS_USERNAME_SESSION_KEY'], None)
+    if net_id is not None:
+        data = {"net_id": net_id}
+        return render_template('search.html', data=data)
+    else:
+        return app.send_static_file('intro.html')
+
+
+@app.route('/about')
+def about():
+    return app.send_static_file('about.html')
 
 
 @app.route('/photos/<path:filename>')
@@ -67,95 +88,6 @@ def after_login():
         return render_template('my_profile.html', data=data)
 
 
-@app.route('/createprofile', methods=['POST'])
-def create_user():
-    """
-    Processes form data in POST request from profile creation page and adds the user to the database table Profile.
-
-    The profile photo associated with the user is recorded in the column "photo" in the Profile table.
-    It is stored as <hash>.<file_extension> where <hash> is a hash is a hash of the uploaded photo and <file_extension> is...well, the file extension.
-    The actual photo file is stored as app/photos/<hash>.<file_extension>
-    """
-    # Fields from form
-    fields = ["net_id", "name", "year", "dob", "college", "gender", "bio"]
-    # Get user-entered values from form
-    values = {}
-    for field in fields:
-        values[field] = request.form[field]
-    # Uploaded profile photo
-    photo = request.files['photo']
-    # Generate a string representation of a hash of the photo
-    # This associates the local copy of the file with the user in the database
-    photo_hash = str(hash(photo))
-    # Create a new user from the Profile model
-    if photo:
-        user = Profile(values["net_id"], values["name"], values["year"], values["dob"], values["college"], values["gender"], values["bio"], photo_hash + "." + file_extension(photo.filename))
-    else:
-        user = Profile(values["net_id"], values["name"], values["year"], values["dob"], values["college"], values["gender"], values["bio"])
-    # The user selected a photo of an invalid file extension
-    # Redirect the user to an error page
-    if photo and not allowed_file(photo.filename):
-        data = {"net_id": values["net_id"], "name": values["name"], "year": values["year"], "dob": values["dob"], "college": values["college"], "gender": values["gender"], "bio": values["bio"], "error": "Error: invalid photo file extension ." + str(file_extension(photo.filename))}
-        return render_template('profile_creation.html', data=data)
-    # Store the user's profile photo on the server
-    if photo and allowed_file(photo.filename):
-        filename = secure_filename(photo_hash) + "." + file_extension(photo.filename)
-        photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    # Add this new user to the database
-    db.session.add(user)
-    db.session.commit()
-    data = {"net_id": values["net_id"], "profile": user}
-    return render_template('my_profile.html', data=data)
-
-
-@app.route('/updateprofile', methods=['POST'])
-def update_user():
-    """
-    Called when the user is modifying is/her account from the My Profile page.
-    Updates the database to reflect the user's changes.
-    """
-    # Retrieve the user by net ID from the database
-    user = Profile.query.filter_by(net_id=request.form["net_id"]).first()
-    # Update all the columns
-    user.name = request.form["name"]
-    user.year = request.form["year"]
-    user.dob = request.form["dob"]
-    user.college = request.form["college"]
-    user.gender = request.form["gender"]
-    user.bio = request.form["bio"]
-    # Update the photo only if the user has changed the picture
-    photo = request.files['photo']
-    if photo and allowed_file(photo.filename):
-        photo_hash = str(hash(request.files['photo']))
-        filename = secure_filename(photo_hash) + "." + file_extension(photo.filename)
-        photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        user.photo = photo_hash + "." + file_extension(photo.filename)
-    # Commit the changes
-    db.session.merge(user)
-    db.session.commit()
-    # Pass along the data after refresh
-    data = {"net_id": user.net_id, "profile": user}
-    return render_template('my_profile.html', data=data)
-
-
-@app.route('/deleteprofile', methods=['GET', 'POST'])
-def delete_user():
-    """
-    Removes the user from the database.
-    """
-    # Get Net ID from GET data
-    net_id = request.args.get('net_id', '')
-    # Security measure - make sure the Net ID of the user whose deletion was requested
-    # matches with the Net ID of the user currently logged in
-    # This is a pretty lame security measure in all truth
-    if session.get(app.config['CAS_USERNAME_SESSION_KEY'], None) == net_id:
-        user = Profile.query.filter_by(net_id=net_id).first()
-        db.session.delete(user)
-        db.session.commit()
-    # Logout the session after profile deletion.
-    return redirect("/logout", code=302)
-
-
 def get_user_details(net_id):
     """
     Look up the passed net ID in the Rice online directory and attempt to find
@@ -187,54 +119,7 @@ def get_user_details(net_id):
                 college = line.strip()[9:]
             except:
                 print("Error getting college")
-    return (name, year, college)
-
-
-def allowed_file(filename):
-    """
-    Check if the file is of a permissible file extension.
-    """
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-def file_extension(filename):
-    """
-    Get the file extension of file. Excludes the "." before the extension.
-    """
-    return filename.rsplit('.', 1)[1]
-
-
-@app.route('/', methods=['GET'])
-def index():
-    """
-    First check if the user is logged in. If so, redirect him/her to the main search page.
-    If not, redirect him/her to the intro page.
-    """
-    net_id = session.get(app.config['CAS_USERNAME_SESSION_KEY'], None)
-    if net_id is not None:
-        data = {"net_id": net_id}
-        return render_template('search.html', data=data)
-    else:
-        return app.send_static_file('intro.html')
-
-
-@app.route('/about')
-def about():
-    return app.send_static_file('about.html')
-
-
-@app.route('/delete_account')
-def delete_account():
-    """
-    Delete user account page.
-    """
-    net_id = session.get(app.config['CAS_USERNAME_SESSION_KEY'], None)
-    if net_id is not None:
-        data = {"net_id": net_id}
-        return render_template('delete_account.html', data=data)
-    else:
-        # Redirect user to intro
-        index()
+    return name, year, college
 
 
 @app.route('/my_profile')
@@ -248,6 +133,20 @@ def my_profile():
         data = {"net_id": net_id, "profile": user}
         return render_template('my_profile.html', data=data)
     else:
+        index()
+
+
+@app.route('/delete_account')
+def delete_account():
+    """
+    Delete user account page.
+    """
+    net_id = session.get(app.config['CAS_USERNAME_SESSION_KEY'], None)
+    if net_id is not None:
+        data = {"net_id": net_id}
+        return render_template('delete_account.html', data=data)
+    else:
+        # Redirect user to intro
         index()
 
 
