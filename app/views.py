9cd2ff9b5b2b7b64778e3a15f37b9f7028f3d672
Kevin Lin <developer@kevinlin.info>
2014-12-23 12:56:55 -0800

    User profile page - user not found error handling

--- app/views.py
+++ app/views.py
@@ -205,14 +205,9 @@ def user_profile(path):
     user = Profile.query.filter_by(net_id=path).first()
     # Stylistic typographic choices: uppercase and lowercase versions
     # I'm sure there's an easier way to do this
-    data = {}
-    data["profile"] = user
-    data["net_id"] = path
+    data = {"profile": user, "net_id": path, "net_id_uppercase": path.upper(), "net_id_lowercase": path.lower(), "logged_in_net_id": login}
     # Meh; too many net ID's to keep track of...
-    data["logged_in_net_id"] = login
-    if user is not None:
-        data["net_id_uppercase"] = path.upper()
-        data["net_id_lowercase"] = path.lower()
+    if user:
         data["name_uppercase"] = user.name.upper()
         data["name_lowercase"] = user.name.lower()
         data["name_first_uppercase"] = str(user.name).split()[0].upper()
@@ -221,10 +216,7 @@ def user_profile(path):
         data["year_uppercase"] = user.year.upper()
         data["year_lowercase"] = user.year.lower()
         data["age"] = compute_age(user.dob)
-        return render_template('user.html', data=data)
-    else:
-        # If user doesn't exist, profile key will map to None.
-        return render_template('user_not_exists.html', data=data)
+    return render_template('user.html', data=data)
 
 
 def compute_age(dob):
